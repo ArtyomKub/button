ada2a56bdfab4e925e60cf856cfb895218901103
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-20 09:54:17 +0200

    Add onClick-event

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <button>My YouTube Chanel - 1</button>
+            <button onClick={(event) => {
+                console.log('Working')
+            }}>My YouTube Chanel - 1
+            </button>
         </div>
     );
 }
