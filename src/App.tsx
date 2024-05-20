0558782f8a985b372cb010156d6bac12fb995254
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-20 13:35:01 +0200

    Commentig all Code in App.tsx. Create a new funcion App with two button. I do this for made a universal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import React, {MouseEvent} from 'react';
+import React from 'react';
 import './App.css';
 
+// function App() {
+// const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+//     console.log('Hi, I am Artsem')
+// }
+//
+// const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+//     console.log('Hi, I am Batman')
+// }
+// const onClickHandler = (name:string) => {
+//     console.log(name)
+// }
+// const for1 = () => {
+//     console.log(100200)
+// }
+//
+// const for2 = (zero: number) => {
+//     console.log(zero)
+// }
+//
+//     return (
+//         <div className="App">
+//             {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('MASK')}>My YouTube Chanel - 1</button>*/}
+//             {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('ILON')}>My YouTube Chanel - 2</button>*/}
+//
+//             <button onClick={for1}>number one</button>
+//             <button onClick={(event: MouseEvent<HTMLButtonElement>) => for2(100200)}>number two</button>
+//         </div>
+//     );
+// }
+
 function App() {
-    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hi, I am Artsem')
-    // }
-    //
-    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hi, I am Batman')
-    // }
-    // const onClickHandler = (name:string) => {
-    //     console.log(name)
-    // }
-
-    const for1 = () => {
-        console.log('100200')
-    }
-
-    const for2 = (zero: number) => {
-        console.log(zero)
-    }
-
     return (
-        <div className="App">
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('MASK')}>My YouTube Chanel - 1</button>*/}
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('ILON')}>My YouTube Chanel - 2</button>*/}
-
-            <button onClick={for1}>number one</button>
-            <button onClick={(event: MouseEvent<HTMLButtonElement>) => for2(100200)}>number two</button>
+        <div className={'App'}>
+            <button>MyYouTubeChanel-1</button>
+            <button>MyYouTubeChanel-2</button>
         </div>
-    );
+    )
 }
+
 
 export default App;
