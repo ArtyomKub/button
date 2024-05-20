3e29a83cd3ba8cfac115ee1271b944c797e02f20
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-20 13:29:26 +0200

    Create two button tag and two component (for1, for2). In buttons tag connect event

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,21 @@ function App() {
     //     console.log(name)
     // }
 
+    const for1 = () => {
+        console.log('100200')
+    }
+
+    const for2 = (zero: number) => {
+        console.log(zero)
+    }
+
     return (
         <div className="App">
             {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('MASK')}>My YouTube Chanel - 1</button>*/}
             {/*<button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('ILON')}>My YouTube Chanel - 2</button>*/}
 
-
+            <button onClick={for1}>number one</button>
+            <button onClick={(event: MouseEvent<HTMLButtonElement>) => for2(100200)}>number two</button>
         </div>
     );
 }
