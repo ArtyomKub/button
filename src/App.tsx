e368d1d098a6bc750a73fefe91edc70a98ce8c49
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-20 13:11:30 +0200

    Create component onClickHandler and connect in tag 'button'

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-    const MyFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-        console.log('Hi, I am Artsem')
-    }
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hi, I am Artsem')
+    // }
+    //
+    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hi, I am Batman')
+    // }
 
-    const MySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-        console.log('Hi, I am Batman')
+    const onClickHandler = (name:string) => {
+        console.log(name)
     }
 
     return (
         <div className="App">
-            <button onClick={MyFirstSubscriber}>My YouTube Chanel - 1</button>
-            <button onClick={MySecondSubscriber}>My YouTube Chanel - 2</button>
+            <button onClick={()=>{onClickHandler('Mask')}}>My YouTube Chanel - 1</button>
+            <button onClick={()=>{onClickHandler('Ilon')}}>My YouTube Chanel - 2</button>
         </div>
     );
 }
